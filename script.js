03fb4804793b416892044fad0f37c82568d07a4a
Luciano Navarro <l.navarro2022@icloud.com>
2026-09-12 16:37:07 +0200

    function operate

--- script.js
+++ script.js
@@ -17,3 +17,13 @@ const divide = function (a, b){
 }
 
 console.log(multiply(2, 2))
+
+
+let operate = function (operator, number1, number2) {
+
+};
+
+
+let number1 = ""
+let operator = ""
+let number2 = ""
